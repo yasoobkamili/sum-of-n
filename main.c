c6b5e1efaefc16ef3320929f30b624937f7184eb
yasoobkamili <kamiliyasoob@gmail.com>
2024-06-26 10:16:22 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  /*
+  int sum,n;
+  printf("Enter n");
+  scanf("%d",&n);
+  sum= (n*(n+1))/2;
+  printf("sum of first %d natural no.'s is %d",n,sum);
+  */
+
+
+  
+  // using for loop
+  
+  int i,n,sum=0;
+  printf("Enter n");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+   sum= sum+i;
+  }
+  printf("%d",sum);
+  
+    return 0;
+
+}
