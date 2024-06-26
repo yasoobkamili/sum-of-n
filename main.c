6768923c25a54c850346037707b1c2d1d4c390e4
yasoobkamili <kamiliyasoob@gmail.com>
2024-06-26 16:04:33 +0530

    main.c

--- main.c
+++ main.c
@@ -12,16 +12,12 @@ int main(void) {
 
   
   // using for loop
-  
   int i,n,sum=0;
   printf("Enter n");
   scanf("%d",&n);
   for(i=1;i<=n;i++)
-  {
-   sum= sum+i;
-  }
+    sum= sum+i;
   printf("%d",sum);
-  
-    return 0;
+  return 0;
 
 }
