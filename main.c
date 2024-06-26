78637b55ae78cf8ac81c9741d7800f6188ca8a06
yasoobkamili <kamiliyasoob@gmail.com>
2024-06-26 16:05:57 +0530

    main.c

--- main.c
+++ main.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
-
-int main(void) {
-  /*
-  int sum,n;
-  printf("Enter n");
-  scanf("%d",&n);
-  sum= (n*(n+1))/2;
-  printf("sum of first %d natural no.'s is %d",n,sum);
-  */
-
-
-  
+int main(void)
+{
   // using for loop
   int i,n,sum=0;
   printf("Enter n");
@@ -18,6 +8,16 @@ int main(void) {
   for(i=1;i<=n;i++)
     sum= sum+i;
   printf("%d",sum);
+
+
+  /*
+  //using formula
+  int sum,n;
+  printf("Enter n");
+  scanf("%d",&n);
+  sum= (n*(n+1))/2;
+  printf("sum of first %d natural no.'s is %d",n,sum);
+  */
   return 0;
 
 }
